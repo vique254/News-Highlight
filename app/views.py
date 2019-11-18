7a6fae23f15def1344519611eca2a282b290d574
vique254 <victoronyango051@gmail.com>
2019-11-18 08:01:18 +0300

    create a news template and a function in views.py

--- app/views.py
+++ app/views.py
@@ -7,4 +7,12 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'NEWS'
+    return render_template('index.html', message = message)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    view news page function that returns the news highlight page and its data
+    '''
+    return render_template('news.html',id = news_id)
